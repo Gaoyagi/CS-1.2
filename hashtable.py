0101cd2aa86b6ed0fd16cc4d53e184f6f8e79433
George Aoyagi <aoyagigeorge@gmail.com>
2020-03-04 21:05:46 -0800

    finished hashtable

--- hashtable.py
+++ hashtable.py
@@ -1,6 +1,6 @@
 #!python
 
-from linkedlist import LinkedList
+from linkedlist import LinkedList, Node
 
 
 class HashTable(object):
@@ -22,10 +22,11 @@ class HashTable(object):
         return 'HashTable({!r})'.format(self.items())
 
     #hashing function
-    def _bucket_index(self, key):
+    def _bucket_index(self, value):
         """Return the bucket index where the given key would be stored."""
         # Calculate the given key's hash code and transform into bucket index
-        return hash(key) % len(self.buckets)
+        key = hash(value)
+        return key % len(self.buckets)
 
     #creates an list that holds all the keys in the hashtable
     def keys(self):
@@ -35,7 +36,7 @@ class HashTable(object):
         all_keys = []   #creates key's list
         #loops through all the hashtables buckets
         for bucket in self.buckets:
-            for key, value in bucket.items():
+            for key, _ in bucket.items():
                 all_keys.append(key)
         return all_keys
 
@@ -47,7 +48,7 @@ class HashTable(object):
         for bucket in self.buckets:
             for _, value in bucket.items():
                 values.append(value)
-        return value
+        return values
 
     def items(self):
         """Return a list of all items (key-value pairs) in this hash table.
@@ -85,7 +86,7 @@ class HashTable(object):
         # TODO: If found, return value associated with given key
         # TODO: Otherwise, raise error to tell user get failed
         # Hint: raise KeyError('Key not found: {}'.format(key))
-        index = _bucket_index(key)      
+        index = self._bucket_index(key)      
         #check if the key exists in the hashtable
         if self.buckets[index] == None:
             raise KeyError('Key not found: {}'.format(key))
@@ -99,18 +100,19 @@ class HashTable(object):
     def set(self, key, value):
         """Insert or update the given key with its associated value.
         TODO: Running time: O(???) Why and under what conditions?"""
-        # TODO: Find bucket where given key belongs
-        # TODO: Check if key-value entry exists in bucket
-        # TODO: If found, update value associated with given key
-        # TODO: Otherwise, insert given key-value entry into bucket
-        index = _bucket_index(key)
-        entry =  {key: value}
-        if self.buckets[index] == None:
-            self.buckets[index].head.data = Node(entry)
-            self.buckets[index].tail = self.buckets[index].head.data
-        else:
-            self.buckets[index].tail.next = Node(entry)
-            self.buckets[index].tail = self.buckets[index].tail.next
+        index = self._bucket_index(key)             #get hashtable bucket index
+        entry = (key, value)                        #create the entry to be inserted
+        if self.buckets[index].head != None:        #check if the linked list is empty
+            #if linked list isnt empty check to see if theres  an entry already with the same key and replace the value
+            iterate = self.buckets[index].head
+            while iterate !=  None:
+                if iterate.data[0] == key:
+                    iterate.data = entry
+                    return
+                iterate = iterate.next
+        entry = (key, value)                #create the entry to be inserted
+        self.buckets[index].append(entry)   #add entry to the 
+       
 
     def delete(self, key):
         """Delete the given key from this hash table, or raise KeyError.
@@ -120,13 +122,38 @@ class HashTable(object):
         # TODO: If found, delete entry associated with given key
         # TODO: Otherwise, raise error to tell user delete failed
         # Hint: raise KeyError('Key not found: {}'.format(key))
-        index = _bucket_index(key)
-        if self.buckets[index] == None:
+        index = self._bucket_index(key)
+        if self.buckets[index].head == None:
             raise KeyError('Key not found: {}'.format(key))
-        iterate = self.buckets[index].head
-        while iterate.next != None:
-            if iterate.data
-        raise KeyError('Key not found: {}'.format(key))
+
+        #check if the head is the desired node
+        if self.buckets[index].head.data[0] == key:
+            self.buckets[index].head = self.buckets[index].head.next  #sets head to the next node, deletes prev head
+            #edge case if that was the last node
+            if self.buckets[index].head == None:
+                self.buckets[index].tail = self.buckets[index].head
+            return
+        iterator = self.buckets[index].head
+        found = False
+        #loops through list and stops on the node right before the desired node
+        while iterator.next != None:
+            #check if next node has the desired data
+            if iterator.next.data[0] == key:
+                found = True
+                break
+            iterator = iterator.next    #go to next node
+        if found:
+            #edge case for if the item youre looking for is the last item
+            if iterator.next == self.buckets[index].tail:
+                self.buckets[index].tail = iterator     #set the Linked lists tail to the 2nd last to node
+                iterator.next = None                    #eliminate the iterators tail
+                return
+            #makes the node right before next pointer equal to the node right after the desired node then deletes it    
+            temp = iterator.next
+            iterator.next = temp.next
+            del temp
+        else:
+            raise KeyError('Key not found: {}'.format(key))
 
 
 def test_hash_table():
@@ -162,3 +189,4 @@ def test_hash_table():
 
 if __name__ == '__main__':
     test_hash_table()
+    
